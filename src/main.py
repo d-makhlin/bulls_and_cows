2533ac02331c35295dcfbf5037d30e62cd281018
Дмитрий Махлин <d.makhlin@softpro.com>
2020-04-29 21:10:19 +0300

    send_message

--- src/main.py
+++ src/main.py
@@ -1,14 +1,21 @@
 import telebot
 
-from src.messages import HELLO_MESSAGE
+from src.messages import HELLO_MESSAGE, RULES_MESSAGE
 
-BOT_TOKEN = '1126940255:AAGXar8Z0FYnzWRGNrjHsRY50vJbx08OA-c'
+BOT_TOKEN = ''
 bot = telebot.TeleBot(BOT_TOKEN)
 
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, HELLO_MESSAGE)
+    start_keyborad = telebot.types.ReplyKeyboardMarkup('Начать игру', 'Правила')
+    bot.send_message(message.chat.id, HELLO_MESSAGE, reply_markup=start_keyborad)
+
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text.lower() == 'правила':
+        bot.send_message(message.chat.id, RULES_MESSAGE)
 
 
 bot.polling()
